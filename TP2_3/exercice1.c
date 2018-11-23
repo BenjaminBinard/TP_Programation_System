82cb3a374c9f0db3eb3ba67bedf3500e5f727106
Benjamin BINARD <benjii.binard@gmail.com>
2018-11-23 11:50:10 +0100

    modifié :         TP2_3/exercice1.c

--- TP2_3/exercice1.c
+++ TP2_3/exercice1.c
@@ -8,30 +8,6 @@
 int tubpf[2];
 int tubfp[2];
 
-/*void pere( ){
-  //char* chaineLue = (char*)malloc(80);
-  char* chaine="Salut oh toi fils !!!";
-  char * lecture = (char*)malloc(80);
-  read(tubfp[0], lecture, 1 );
-  printf("(du fils) : %d\n", lecture);
-  write(tubpf[1], chaine, strlen(chaine));
-  close(tubfp[0]);
-  close(tubpf[1]);
-}
-
-void fils( ){
-  char* chaineLue= (char*)malloc(80);
-  srand(time(NULL));
-  char * nbr = rand();
-  printf("%d\n",nbr);
-  write(tubfp[1], nbr, strlen(nbr));
-  read(tubpf[0], chaineLue, 21 );
-  printf("(du père) : %s\n", chaineLue);
-  close(tubpf[0]);
-  close(tubfp[1]);
-  exit(0);
-}*/
-
 void pere( ) {
   char*chaineLue= (char*)malloc(80);
   char*chaine= (char*)malloc(80);
